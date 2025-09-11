6259da9a68c0d381edbd23bc21acf7109a731df0
Lahari Banukunta <152053025+LahariBanukunta@users.noreply.github.com>
2025-09-11 19:33:14 +0530

    Update RC4.java

--- RC4.java
+++ RC4.java
@@ -1,22 +1,25 @@
-import javax.crypto.*;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
-public class AES_CBC_Simple {
+public class SimpleRC4 {
     public static void main(String[] args) throws Exception {
-        SecretKey key = KeyGenerator.getInstance("AES").generateKey(); // Generate random AES key
-        IvParameterSpec iv = new IvParameterSpec(new byte[16]); // 16-byte zero IV
-        String text = "Hello CBC Mode AES!";
+        String key = "SecretKey";
+        String text = "Hello RC4!";
 
-        // Encrypt & print in one line
-        Cipher encCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        encCipher.init(Cipher.ENCRYPT_MODE, key, iv);
-        String encrypted = Base64.getEncoder().encodeToString(encCipher.doFinal(text.getBytes()));
-        System.out.println("Encrypted: " + encrypted);
+        SecretKeySpec rc4Key = new SecretKeySpec(key.getBytes(), "RC4");
+        Cipher cipher = Cipher.getInstance("RC4");
 
-        // Decrypt & print in one line
-        Cipher decCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        decCipher.init(Cipher.DECRYPT_MODE, key, iv);
-        System.out.println("Decrypted: " + new String(decCipher.doFinal(Base64.getDecoder().decode(encrypted))));
+        // Encrypt in one line
+        cipher.init(Cipher.ENCRYPT_MODE, rc4Key);
+        System.out.println("Encrypted: " +
+                Base64.getEncoder().encodeToString(cipher.doFinal(text.getBytes())));
+
+        // Decrypt in one line
+        cipher.init(Cipher.DECRYPT_MODE, rc4Key);
+        System.out.println("Decrypted: " +
+                new String(cipher.doFinal(Base64.getDecoder().decode(
+                        Base64.getEncoder().encodeToString(cipher.doFinal(text.getBytes())))
+                )));
     }
 }
