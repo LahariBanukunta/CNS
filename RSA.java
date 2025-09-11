cebd43da7e1fd7a1450ca4f7501cdeaa77925d06
Lahari Banukunta <152053025+LahariBanukunta@users.noreply.github.com>
2025-09-11 19:35:16 +0530

    Update RSA.java

--- RSA.java
+++ RSA.java
@@ -3,32 +3,22 @@ import javax.crypto.Cipher;
 import java.util.Base64;
 
 public class SimpleRSA {
-
     public static void main(String[] args) throws Exception {
-        // Generate RSA key pair (2048 bits)
+        // Generate RSA key pair
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         keyGen.initialize(2048);
         KeyPair keyPair = keyGen.generateKeyPair();
 
-        PublicKey publicKey = keyPair.getPublic();
-        PrivateKey privateKey = keyPair.getPrivate();
+        String text = "Hello RSA Encryption!";
 
-        String plaintext = "Hello RSA Encryption!";
+        // Encrypt + print in one line
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
+        String encrypted = Base64.getEncoder().encodeToString(cipher.doFinal(text.getBytes()));
+        System.out.println("Encrypted (Base64): " + encrypted);
 
-        // Encrypt with public key
-        Cipher encryptCipher = Cipher.getInstance("RSA");
-        encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] encryptedBytes = encryptCipher.doFinal(plaintext.getBytes());
-
-        String encryptedBase64 = Base64.getEncoder().encodeToString(encryptedBytes);
-        System.out.println("Encrypted (Base64): " + encryptedBase64);
-
-        // Decrypt with private key
-        Cipher decryptCipher = Cipher.getInstance("RSA");
-        decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] decryptedBytes = decryptCipher.doFinal(encryptedBytes);
-
-        String decrypted = new String(decryptedBytes);
-        System.out.println("Decrypted: " + decrypted);
+        // Decrypt + print in one line
+        cipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
+        System.out.println("Decrypted: " + new String(cipher.doFinal(Base64.getDecoder().decode(encrypted))));
     }
 }
