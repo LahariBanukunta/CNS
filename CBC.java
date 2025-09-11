9f01f45b3affd3d95bd064a8754293634a5c5f56
Lahari Banukunta <152053025+LahariBanukunta@users.noreply.github.com>
2025-09-11 19:23:06 +0530

    Update CBC.java

--- CBC.java
+++ CBC.java
@@ -1,48 +1,27 @@
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
-import java.security.SecureRandom;
 import java.util.Base64;
 
 public class AES_CBC {
+    static IvParameterSpec iv = new IvParameterSpec(new byte[16]); // fixed IV (all zeros)
 
-    // Generate random IV
-    private static IvParameterSpec generateIV() {
-        byte[] iv = new byte[16]; // AES block size = 16 bytes
-        new SecureRandom().nextBytes(iv);
-        return new IvParameterSpec(iv);
+    static String enc(String text, SecretKey key) throws Exception {
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE, key, iv);
+        return Base64.getEncoder().encodeToString(c.doFinal(text.getBytes()));
     }
 
-    public static String encrypt(String plainText, SecretKey key, IvParameterSpec iv) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding"); // CBC mode
-        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-        byte[] encrypted = cipher.doFinal(plainText.getBytes());
-        return Base64.getEncoder().encodeToString(encrypted);
+    static String dec(String text, SecretKey key) throws Exception {
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.DECRYPT_MODE, key, iv);
+        return new String(c.doFinal(Base64.getDecoder().decode(text)));
     }
 
-    public static String decrypt(String cipherText, SecretKey key, IvParameterSpec iv) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, key, iv);
-        byte[] decoded = Base64.getDecoder().decode(cipherText);
-        byte[] decrypted = cipher.doFinal(decoded);
-        return new String(decrypted);
-    }
-
-    public static void main(String[] args) throws Exception {
-        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        keyGen.init(128);
-        SecretKey key = keyGen.generateKey();
-
-        IvParameterSpec iv = generateIV();
-
-        String message = "Hello CBC Mode AES!";
-        String encrypted = encrypt(message, key, iv);
-        String decrypted = decrypt(encrypted, key, iv);
-
-        System.out.println("CBC Mode:");
-        System.out.println("Encrypted: " + encrypted);
-        System.out.println("Decrypted: " + decrypted);
-        System.out.println("IV (Base64): " + Base64.getEncoder().encodeToString(iv.getIV()));
+    public static void main(String[] a) throws Exception {
+        SecretKey key = KeyGenerator.getInstance("AES").generateKey();
+        String msg = "Hello CBC Mode AES!";
+        String e = enc(msg, key);
+        System.out.println("Encrypted: " + e);
+        System.out.println("Decrypted: " + dec(e, key));
     }
 }
